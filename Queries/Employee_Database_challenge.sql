b7c020e88bb993cc30a1baba7cb1e940120d813f
Katie Hopkins <HopkinsKV@gmail.com>
2021-11-30 21:40:09 -0600

    Query typo updt

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -10,8 +10,8 @@ SELECT e.emp_no,
 INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as ttl
-	ON e.emp_no = t.emp_no
-	WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	ON e.emp_no = ttl.emp_no
+	WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY e.emp_no;
 
  --Create unique_titles (show emps most recent job title)
@@ -43,10 +43,9 @@ SELECT DISTINCT ON (e.emp_no)
 INTO mentorship_eligibility
 FROM employees as e
 INNER JOIN dept_emp AS de
-	ON (e.emp_no = de.emp_no)
+	ON e.emp_no = de.emp_no
 INNER JOIN titles as ttl
-	ON (e.emp_no = ttl.emp_no)
+	ON e.emp_no = ttl.emp_no
 WHERE (de.to_date = ('9999-01-01'))
 	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
-
